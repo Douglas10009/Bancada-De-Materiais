8e8225f2e8ec37e54cc72e468b3ec55c0d9afc23
Douglas10009 <douglasevertonev@gmail.com>
2022-08-01 20:26:19 -0300

    inicio projeto

---
+++ Java/Logisticas-CIA/index.java
@@ -0,0 +1,8 @@
+public class index {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------");
+
+        System.out.println("Seja bem vindo a Logistica e CIA :)");
+        
+    }
+}
