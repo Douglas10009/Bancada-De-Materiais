97414fa57509ded1b0d592abab03c1c184860f49
Douglas10009 <douglasevertonev@gmail.com>
2022-08-03 19:39:59 -0300

    atualiza;áo

--- Java/Logisticas-CIA/index.java
+++ Java/Logisticas-CIA/index.java
@@ -4,5 +4,15 @@ public class index {
 
         System.out.println("Seja bem vindo a Logistica e CIA :)");
         
+        System.out.println("Escolha uma opção:");
+        System.out.println("1- Sistema de logistica");
+        System.out.println("2 - Sistema de controle");
+        System.out.println("3 - Sistema de atualização de dados");
+        System.out.println("4 - Sistema de transação");
+        System.out.println("5 - Relatório");
+
+        System.out.println();
+
     }
 }
+
