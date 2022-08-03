5b11e1f8b2ef880751440095d542f8a29093916f
Douglas10009 <douglasevertonev@gmail.com>
2022-08-03 20:26:00 -0300

    atualização fornecedores

---
+++ Java/Logisticas-CIA/Fornecedores.java
@@ -0,0 +1,8 @@
+public class Fornecedores {
+    //nome, endereço, cnpj, telefone, código do fornecedor
+    public String nome = ""; //Provável vai ter q mudar pq public é "falha" de segurança
+    public String endereco = "";
+    public int cnpj = 0;
+    public int telefone = 0;
+    public int cod_fornecedor = 0;
+}
--- Java/Logisticas-CIA/index.java
+++ Java/Logisticas-CIA/index.java
@@ -1,18 +1,52 @@
+import java.util.Scanner;
+
 public class index {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("--------------------------------------");
 
-        System.out.println("Seja bem vindo a Logistica e CIA :)");
-        
-        System.out.println("Escolha uma opção:");
-        System.out.println("1- Sistema de logistica");
-        System.out.println("2 - Sistema de controle");
-        System.out.println("3 - Sistema de atualização de dados");
-        System.out.println("4 - Sistema de transação");
-        System.out.println("5 - Relatório");
+        System.out.println("--> Seja bem vindo a Logistica e CIA :)");
 
-        System.out.println();
+        int opcao = 0;
+        while (opcao != 6) {
+
+            System.out.println("Escolha uma opção:");
+            System.out.println("1- Sistema de logistica");
+            System.out.println("2 - Sistema de controle");
+            System.out.println("3 - Sistema de atualização de dados");
+            System.out.println("4 - Sistema de transação");
+            System.out.println("5 - Relatório");
+            System.out.println("6 - Sair");
+
+            System.out.print("Digite o número da sua opção: ");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1: // Logistica
+                    // Cadastrar novos fornecedores
+                    // Cadastrar Clientes
+                    // Cadastrar Produtos
+                    break;
+                case 2:// Controle
+
+                    break;
+                case 3: // Atualização de dados
+
+                    break;
+                case 4: // Sistema de transação
+
+                    break;
+                case 5: // Relatório
+
+                    break;
+                case 6: // Saída
+
+                    break;
+                default:
+                    break;
+            }
+        }
+        sc.close();
 
     }
 }
-
