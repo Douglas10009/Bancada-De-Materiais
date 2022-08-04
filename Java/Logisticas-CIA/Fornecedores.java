8297ba8e279da8e5a92f75ff5e0fc532c45b0cb6
Douglas10009 <douglasevertonev@gmail.com>
2022-08-04 17:32:34 -0300

    Alterações no index e no fornecedores

--- Java/Logisticas-CIA/Fornecedores.java
+++ Java/Logisticas-CIA/Fornecedores.java
@@ -1,8 +1,17 @@
-public class Fornecedores {
+public class Fornecedores  {
     //nome, endereço, cnpj, telefone, código do fornecedor
-    public String nome = ""; //Provável vai ter q mudar pq public é "falha" de segurança
-    public String endereco = "";
-    public int cnpj = 0;
-    public int telefone = 0;
-    public int cod_fornecedor = 0;
+
+    
+    private String nome = ""; //Provável vai ter q mudar pq public é "falha" de segurança
+    private String endereco = "";
+    private int cnpj = 0;
+    private int telefone = 0;
+    private int cod_fornecedor = 0;
+
+    public Fornecedores (String nome, int cnpj, String endereco, int telefone, int cod_fornecedor){
+        this.nome = nome;
+        this.cnpj = cnpj;
+        this.telefone = telefone;
+        this.cod_fornecedor = cod_fornecedor;
+    }
 }
--- Java/Logisticas-CIA/index.java
+++ Java/Logisticas-CIA/index.java
@@ -10,7 +10,7 @@ public class index {
         int opcao = 0;
         while (opcao != 6) {
 
-            System.out.println("Escolha uma opção:");
+            System.out.println("Escolha uma opção -");
             System.out.println("1- Sistema de logistica");
             System.out.println("2 - Sistema de controle");
             System.out.println("3 - Sistema de atualização de dados");
@@ -24,9 +24,39 @@ public class index {
             switch (opcao) {
                 case 1: // Logistica
                     int opcao_log = 0;
-                    while (opcao_log != 0) {
-                        System.out.println("Escolha uma opção: ");
+                    int contador = 0; // Para saber se é a primeira vez que o usuário entra no menu
 
+                    if (contador == 0) {
+                        // Runtime.getRuntime().exec("cls"); //Tentativa de limpar a tela
+                        clearScreen();
+                        System.out.println("-- Sistema de logistica --");
+                        System.out.println("Escolha uma opção -");
+                        System.out.println("1 - Cadastrar fornecedor");
+                        System.out.println("2 - Voltar");
+                        System.out.print("Digite o número da sua opção: ");
+                        opcao_log = sc.nextInt();
+
+                        switch (opcao_log) {
+                            case 1:
+                                System.out.println("Digite o nome do fornecedor: ");
+                                String nome = sc.nextLine();
+                                System.out.println("Digite o endereço do fornecedor: ");
+                                String endereco = sc.nextLine();
+                                System.out.println("Digite o CNPJ do fornecedor: ");
+                                String cnpj = sc.nextLine();
+                                System.out.println("Digite o telefone do fornecedor: ");
+                                int telefone = sc.nextInt();
+
+                                System.out.println("O nome: " + nome);
+                                System.out.println("Endereço: " + endereco);
+                                System.out.println("CNPJ: " + cnpj);
+                                System.out.println("Telefone: " + telefone);
+                                break;
+
+                            default:
+                                break;
+                        }
+                        clearScreen(); //Função de limpar a tela
                     }
                     // Cadastrar novos fornecedores
                     // Cadastrar Clientes
@@ -48,10 +78,25 @@ public class index {
 
                     break;
                 default:
+                    System.out.println("Opção inválida, tente novamente. ");
                     break;
             }
         }
         sc.close();
 
     }
+
+    public static void clearScreen() {
+        char esc = 27;
+        String clear = esc + "[2J"; // codigo ansi para limpar a tela
+        System.out.print(clear);
+    }
 }
+
+/*
+ * System.out.println("Escolha uma opção -");
+ * System.out.println("1 - ");
+ * System.out.println("2 -");
+ * System.out.print("Digite o número da sua opção: ");
+ * opcao_ = sc.nextInt();
+ */
