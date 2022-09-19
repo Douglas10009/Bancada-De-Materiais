ef5b69ffe00f3da5035aaf77c6240fe9fdc25deb
Douglas10009 <douglasevertonev@gmail.com>
2022-09-19 15:24:47 -0300

    questão 1

---
+++ Java/Abstratas/swing.java
@@ -0,0 +1,12 @@
+package Abstratas;
+
+public class swing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Interface - CLasse Abstrata
+		
+
+	}
+
+}
---
+++ Java/ListasExercicios/Classe-Abstratas/q1/Carro.java
@@ -0,0 +1,5 @@
+package Listas-Exercicios.Classe-Abstratas.q1;
+
+public class Carro {
+    
+}
